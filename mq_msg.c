b7d68cb0f0d665e82f0cdecc47a424906971dc8d
Shim_ZoMoe <xianszm007@gmail.com>
2014-07-23 14:44:16 +0000

    convert_func

--- mq_msg.c
+++ mq_msg.c
@@ -5,6 +5,78 @@
  * mail  : xianszm007@gmail.com
  * Author: zomoe
  */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "mq_msg.h"
 
-//message issue process
-//
+int buffer_to_header_t(const char *buffer, header_t *header){
+    assert(buffer!=NULL && header!=NULL);
+    header->protocol = *buffer;
+    header->state = convert_buffer_to_int(buffer+1);
+    header->bodylen = convert_buffer_to_long(buffer+1+sizeof(int));
+    return 0;
+}
+
+int header_t_to_buffer(const header_t *header, char *buffer){
+    assert(buffer!=NULL && header!=NULL);
+    *buffer = header->protocol;
+    convert_int_to_buffer(header->state,buffer+1);
+    convert_long_to_buffer(header->bodylen,buffer+1+sizeof(int));
+    return 0;
+}
+
+//the follow functions got from internet ,thanks much !
+
+void convert_int_to_buffer(const int n, char *buffer){
+    unsigned char *p_convert;
+    p_convert = (unsigned char *)buffer;
+    *p_convert++ = (n >> 24) & 0xFF;
+    *p_convert++ = (n >> 16) & 0xFF;
+    *p_convert++ = (n >> 8) & 0xFF;
+    *p_convert++ = n & 0xFF;
+}
+
+
+int convert_buffer_to_int(const char *buffer){
+    int convert_v;
+    convert_v=(
+             ((unsigned char)(*buffer)) << 24) |
+            (((unsigned char)(*(buffer + 1)))<< 16) |
+            (((unsigned char)(*(buffer + 2))) << 8) |
+             ((unsigned char)(*(buffer + 3))
+                )
+            ;
+    return convert_v;
+}
+
+void convert_long_to_buffer(int64_t n, char *buffer){
+    unsigned char *p_convert;
+    p_convert = (unsigned char *)buffer;
+    *p_convert++ = (n >> 56) & 0xFF;
+    *p_convert++ = (n >> 48) & 0xFF;
+    *p_convert++ = (n >> 40) & 0xFF;
+    *p_convert++ = (n >> 32) & 0xFF;
+    *p_convert++ = (n >> 24) & 0xFF;
+    *p_convert++ = (n >> 16) & 0xFF;
+    *p_convert++ = (n >> 8) & 0xFF;
+    *p_convert++ = n & 0xFF;
+}
+
+int64_t convert_buffer_to_long(const char *buffer){
+    unsigned char *p_convert;
+    p_convert = (unsigned char *)buffer;
+    int64_t convert_v;
+    convert_v=(
+             ((int64_t) (*p_convert)) << 56) |
+            (((int64_t) (*(p_convert + 1))) << 48) |
+            (((int64_t) (*(p_convert + 2))) << 40) |
+            (((int64_t) (*(p_convert + 3))) << 32) |
+            (((int64_t) (*(p_convert + 4))) << 24) |
+            (((int64_t) (*(p_convert + 5))) << 16) |
+            (((int64_t) (*(p_convert + 6))) << 8)  |
+             ((int64_t) (*(p_convert + 7))
+                )
+             ;
+}
+
--- mq_msg.h
+++ mq_msg.h
@@ -15,6 +15,7 @@ extern "C":
 #endif
 
 #include <stdint.h>
+#include <sys/types.h>
 #include "mq_types.h"
 
 typedef enum session{
@@ -28,6 +29,18 @@ typedef struct header{
 }header_t;
 
 #define HEADERLEN (sizeof(char) + sizeof(int) + sizeof(int64_t))
+
+void convert_int_to_buffer(const int n, char *buff);
+
+int convert_buffer_to_int(const char *buff);
+
+void convert_long_to_buffer(int64_t n, char *buff);
+
+int64_t convert_buffer_to_long(const char *buff);
+
+int buffer_to_header_t(const char *buffer, header_t *header);
+
+int header_t_to_buffer(const header_t *header, char *buffer);
 
 #ifdef __cplusplus
 }

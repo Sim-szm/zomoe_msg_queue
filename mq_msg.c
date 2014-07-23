881ee130bc70c561757530e45d9b58902b05e343
Shim_ZoMoe <xianszm007@gmail.com>
2014-07-23 13:02:49 +0000

    messge issue ..ing

---
+++ mq_msg.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2014 zomoe
+ *
+ * zomoemq may be copied only under the terms of the GNU General Public License V3
+ * mail  : xianszm007@gmail.com
+ * Author: zomoe
+ */
+
+//message issue process
+//
